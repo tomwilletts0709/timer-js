fb10ee5907cc93ed9b072b0f52b10574f25dc408
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-08-31 12:27:15 +0100

    html fixes

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-
-
 let timerInterval;
+let endtime; 
+let remainingonPause;
 
-const countdownDisplay = document.getElementById("countdownDisplay")
+let focus_ms = 25 * 60 * 1000
+
+const countdownDisplay = document.getElementById("timer")
 
 function startTimer() { 
     clearInterval(timerInterval);
 
-    endtime = Date.now()
+    endtime = Date.now() + (remainingonPause ?? focus_ms)
     remainingonPause = null;
 
     countdown(); 
     timerInterval = setInterval(countdown, 1000);
 
     function countdown() { 
-        const remaining = Math.max(0 , endtime );
-        updateDisplay = remaining();
+        const remaining = Math.max(0, endtime - Date.now());
+        updateDisplay(remaining);
 
         if (remaining === 0) {
             clearInterval(timerInterval);
-        } else 
+        }  
     }
 }
+
+function updateDisplay(ms) {
+  const totalSeconds = Math.ceil(ms / 1000);
+  const mins = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+  const secs = String(totalSeconds % 60).padStart(2, "0");
+  countdownDisplay.textContent = `${mins}:${secs}`;
+}
+
+function stopTimer() {
+        clearInterval(timerInterval);
+        remainingonPause = endtime - Date.now();
+    }
